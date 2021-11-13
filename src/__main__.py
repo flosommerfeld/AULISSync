453aaed9de88283b7c8eb94c009a9a07cb91d622
Florian Sommerfeld <help.flosommerfeld@gmail.com>
2021-11-13 11:41:01 +0100

    add example for clicking on each course

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,11 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys  
 #from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
+from time import sleep
 
 if __name__ == '__main__':
     # TODO change options based on selected/detected browser
@@ -29,4 +33,41 @@ if __name__ == '__main__':
     password_input_elem.send_keys(os.getenv("AULIS_PASSWORD"))
     password_input_elem.send_keys(Keys.RETURN)
 
+
+    # Nach dem Einloggen sollte man auf: https://aulis.hs-bremen.de/ilias.php?baseClass=ilDashboardGUI&cmd=jumpToSelectedItems sein
+
+    # class ilDashboardMainContent beinhaltet alle il-item-title -> child <a> hat link
+    
+    # after the login we are being redirected, so we are waiting until the
+    # dashboard is shown
+    wait = WebDriverWait(driver, 20)
+    element = wait.until(EC.visibility_of_element_located((By.ID, "il_mhead_t_focus")))
+
+
+    if element.get_attribute("text") == "Dashboard":
+        print("we are on the dashboard!")
+    else:
+        print(element.get_attribute("text"))
+    
+    # get elements which hold the courses
+    content = driver.find_element_by_class_name("ilDashboardMainContent")
+    courseElements = content.find_elements_by_class_name("il-item-title")
+ 
+    # iterate over the courses to get their names etc.
+    courses = [course.find_element_by_tag_name("a") for course in courseElements]
+    
+    # convert to set in order to remove duplicates
+    courses = list(set(courses))
+
+    courses_links = [course.get_attribute("href") for course in courses]
+
+    # print the courses/elements which were found
+    print([i.get_attribute("text") for i in courses])
+
+    for link  in courses_links:
+        print("click")
+        driver.get(link)
+        wait = WebDriverWait(driver, 20)
+        driver.execute_script("window.history.go(-1)")
+        wait = WebDriverWait(driver, 20)
     #driver.close()
