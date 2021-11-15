ae5de734416fdc541f49287ab07df60a41f6e389
Florian Sommerfeld <help.flosommerfeld@gmail.com>
2021-11-15 11:14:02 +0100

    update layout and add login to the gui

--- src/__main__.py
+++ src/__main__.py
@@ -240,8 +240,7 @@ class SeleniumIliasWrapper:
         password_input_elem.send_keys(Keys.RETURN)
 
         # wait until we are redirected after the login
-        wait = WebDriverWait(self.driver, 20)
-        heading = wait.until(EC.visibility_of_element_located((By.ID, "il_mhead_t_focus")))
+        heading = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, "il_mhead_t_focus")))
         
         # raise exception if the new page is not the dashboard
         # This can also mean that the authentication was not successful
@@ -341,61 +340,79 @@ def _driver_go_back(driver):
 
 
 def main():
-    menu = ['', ['Show Window', 'Hide Window', '---', '!Disabled Item', 'Change Icon', ['Happy', 'Sad', 'Plain'], 'Exit']]
+    menu = ['', ['Show Window', 'Hide Window', 'Exit']]
     tooltip = 'Tooltip'
 
-    layout = [[sg.Text('My PySimpleGUI Celebration Window - X will minimize to tray')],
-              [sg.T('Double clip icon to restore or right click and choose Show Window')],
-              [sg.T('Icon Tooltip:'), sg.Input(tooltip, key='-IN-', s=(20,1)), sg.B('Change Tooltip')],
+    layout = [[sg.Text("AULIS Login", size =(15, 1), font=40)],
+              [sg.Text("Username", size =(15, 1), font=16),sg.InputText(key='-USERNAME-', size=(25, 1), font=16)],
+              [sg.Text("Password", size =(15, 1), font=16),sg.InputText(key='-PASSWORD-', size=(25, 1), password_char='*', font=16)],
+              [sg.Button("Login"), sg.B("Logout")],
+              [sg.HorizontalSeparator(key='sep')],
+              [sg.Button("Synchronize", disabled=True)],
+              [sg.HorizontalSeparator(key='sep')],
+              [sg.Text("Synchronization Options", size =(25, 1), font=40)],
+              [sg.Text("Courses to sync:")],
+              [sg.Checkbox('BESYST', default=True)],
+              [sg.Text("Other:")],
+              [sg.Checkbox("Live mode", default=False)],
+              [sg.Text("Output Folder:"), sg.In(size=(25,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()],
+              [sg.Listbox(values=[], enable_events=True, size=(60,10),key='-FILE LIST-')],
+              [sg.HorizontalSeparator(key='sep')],
+              [sg.Text("Console", size =(25, 1), font=40)],
               [sg.Multiline(size=(60,10), reroute_stdout=False, reroute_cprint=True, write_only=True, key='-OUT-')],
-              [sg.Button('Go'), sg.B('Hide Icon'), sg.B('Show Icon'), sg.B('Hide Window'), sg.Button('Exit')]]
+              [sg.HorizontalSeparator(key='sep')],
+              [sg.Button('Go'), sg.B('Hide Window'), sg.Button('Exit')]]
 
     window = sg.Window("IliasSync", layout, finalize=True, enable_close_attempted_event=True)
 
+    # Disable login button if logged in
+    window.FindElement("Logout").Update(disabled=True, visible=False)
+    # Disable logout button if not logged in TODO
 
     tray = SystemTray(menu, single_click_events=False, window=window, tooltip=tooltip, icon=sg.DEFAULT_BASE64_ICON)
     sg.cprint(sg.get_versions())
     while True:
         event, values = window.read()
 
-        # IMPORTANT step. It's not required, but convenient. Set event to value from tray
-        # if it's a tray event, change the event variable to be whatever the tray sent
-        if event == tray.key:
-            sg.cprint(f'System Tray Event = ', values[event], c='white on red')
-            event = values[event]       # use the System Tray's event as if was from the window
-
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
 
-        sg.cprint(event, values)
+        sg.cprint(event)
 
         if event in ('Show Window', sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED):
             window.un_hide()
             window.bring_to_front()
-
-            # TODO try logging in the user
-            user = User(os.getenv("AULIS_USERNAME"), os.getenv("AULIS_PASSWORD"))
-            if user.login():
-                sg.cprint("We successfully logged in")
-            else:
-                sg.cprint("Login was not successfull, please check your password")
-                
         elif event in ('Hide Window', sg.WIN_CLOSE_ATTEMPTED_EVENT):
             window.hide()
             tray.show_icon()        # if hiding window, better make sure the icon is visible
             # tray.notify('System Tray Item Chosen', f'You chose {event}')
-        elif event == 'Happy':
-            tray.change_icon(sg.EMOJI_BASE64_HAPPY_JOY)
-        elif event == 'Sad':
-            tray.change_icon(sg.EMOJI_BASE64_FRUSTRATED)
-        elif event == 'Plain':
-            tray.change_icon(sg.DEFAULT_BASE64_ICON)
-        elif event == 'Hide Icon':
-            tray.hide_icon()
-        elif event == 'Show Icon':
-            tray.show_icon()
-        elif event == 'Change Tooltip':
-            tray.set_tooltip(values['-IN-'])
+        elif event == "Login":
+            sg.cprint("Starting the login...")
+            # Grabbing the username and password input
+            user = User(values["-USERNAME-"], values["-PASSWORD-"])
+            # Make the logout button visible if we were successfully logged in
+            if user.login():
+                sg.cprint("We successfully logged in")
+                window.FindElement("Logout").Update(disabled=False, visible=True)
+                window.FindElement("Login").Update(disabled=True, visible=False)
+                window.FindElement("Synchronize").Update(disabled=False)
+                
+            else:
+                sg.cprint("Login was not successful, please check your password!")
+        elif event == "Logout":
+            sg.cprint("You are logged out")
+            # Hide logout button and show login
+            window.FindElement("Logout").Update(disabled=True, visible=False)
+            window.FindElement("Login").Update(disabled=False, visible=True)
+        elif event == '-FOLDER-':                         # Folder name was filled in, make a list of files in the folder
+            folder = values['-FOLDER-']
+            try:
+                file_list = os.listdir(folder)         # get list of files in folder
+            except:
+                file_list = []
+            finally:
+                fnames = [f for f in file_list]
+                window['-FILE LIST-'].update(fnames)
 
     tray.close()            # optional but without a close, the icon may "linger" until moused over
     window.close()
