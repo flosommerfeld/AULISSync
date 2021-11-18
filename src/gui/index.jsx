2e67a77dd5f0543a43bc3b8074e98dc95789d416
Florian Sommerfeld <help.flosommerfeld@gmail.com>
2021-11-18 21:34:32 +0100

    add mui react framework

--- src/gui/index.jsx
+++ src/gui/index.jsx
@@ -1,21 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-/*
-import Header from './components/Header/Header'
-import Editor from './components/Editor/Editor'
-import Ticker from './components/Ticker/Ticker'
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
-import './index.scss'
-*/
 const App = function() {
-  return (
-    <>
-      <h1>Ich bin React - Hallo!</h1>
-    </>
-  )
+  return <Button variant="contained">Hello World</Button>;
 }
 
-const view = App('pywebview')
-
 const element = document.getElementById('app')
-ReactDOM.render(view, element)
+ReactDOM.render(<App />, element)
